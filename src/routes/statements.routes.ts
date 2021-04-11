053b3e52046d08623637961bf661c7214ba6f89b
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-04-11 09:49:23 -0300

    Adicionado controller e rota para transferencia entre contas

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { CreateStatementController } from '../modules/statements/useCases/createStatement/CreateStatementController';
 import { GetBalanceController } from '../modules/statements/useCases/getBalance/GetBalanceController';
 import { GetStatementOperationController } from '../modules/statements/useCases/getStatementOperation/GetStatementOperationController';
+import { TransferBetweenAccountsController } from '../modules/statements/useCases/transferBetweenAccounts/TransferBetweenAccountsController';
 import { ensureAuthenticated } from '../shared/infra/http/middlwares/ensureAuthenticated';
 
 const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
+const transferBetweenAccountsController = new TransferBetweenAccountsController();
 const getStatementOperationController = new GetStatementOperationController();
 
 statementRouter.use(ensureAuthenticated);
@@ -15,6 +17,7 @@ statementRouter.use(ensureAuthenticated);
 statementRouter.get('/balance', getBalanceController.execute);
 statementRouter.post('/deposit', createStatementController.execute);
 statementRouter.post('/withdraw', createStatementController.execute);
+statementRouter.post('/transfer/:user_id', transferBetweenAccountsController.execute);
 statementRouter.get('/:statement_id', getStatementOperationController.execute);
 
 export { statementRouter };
